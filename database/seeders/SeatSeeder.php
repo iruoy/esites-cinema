7efe1b025ea234680a5668b8d74a4b27aedeaacd
Youri Wijnands <youri@wijnands.me>
2022-04-11 09:49:13 +0200

    Add seats

--- database/seeders/SeatSeeder.php
+++ database/seeders/SeatSeeder.php
@@ -2,19 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Seat;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class SeatSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run(): void
     {
-        $this->call([
-            SeatSeeder::class,
-        ]);
+        Seat::factory()
+            ->count(10)
+            ->create();
     }
 }
