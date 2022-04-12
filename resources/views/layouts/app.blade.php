a005e98e3afb96907c53172033621ca619e51bf0
Youri Wijnands <youri@wijnands.me>
2022-04-12 02:44:11 +0200

    Don't load JS except for Livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         {{ $slot }}
     </main>
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+{{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
     @livewireScripts
 </div>
 </body>
